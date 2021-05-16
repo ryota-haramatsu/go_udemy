96300d30c72568dcdf7260e455c6c32c7b3521c9
ryota <haramatsuryota@gmail.com>
2021-05-16 20:44:32 +0900

    logging trading data

--- go_trading/main.go
+++ go_trading/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"gomodtest/go_trading/config"
+	"gomodtest/go_trading/utils"
+	"log"
 )
 
 func main() {
 	fmt.Println(config.Config.ApiKey)
 	fmt.Println(config.Config.ApiSecret)
+	utils.LogginSettings(config.Config.LogFile)
+	log.Println("test")
 }
---
+++ go_trading/utils/logging.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+// ログファイル設定を記述
+func LogginSettings(logFile string) {
+	logfile, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("file=logFile err=%s", err.Error())
+	}
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
+}
