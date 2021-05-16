6ff74a623c06a7c9b80a7e4692706864739fb26f
ryota <haramatsuryota@gmail.com>
2021-05-16 22:08:45 +0900

    GetBalance API

--- go_trading/main.go
+++ go_trading/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"gomodtest/go_trading/bitflyer"
 	"gomodtest/go_trading/config"
 	"gomodtest/go_trading/utils"
-	"log"
 )
 
 func main() {
-	fmt.Println(config.Config.ApiKey)
-	fmt.Println(config.Config.ApiSecret)
+	// fmt.Println(config.Config.ApiKey)
+	// fmt.Println(config.Config.ApiSecret)
 	utils.LogginSettings(config.Config.LogFile)
-	log.Println("test")
+	// log.Println("test")
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	fmt.Println(apiClient.GetBalance())
 }
